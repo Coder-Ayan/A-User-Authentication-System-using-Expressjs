ae7f4d2f4ece9759bb176eef5677b26ac81f1849
Ayan Das Karmakar <coderayan1411@gmail.com>
2023-03-11 16:26:21 +0530

    Created Static Files & Templates

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const path = require('path')
 
 const app = express()
 const port = 3000
 
+// Add Static
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+// Set View Engine
+app.set('view engine', 'njk')
+
+// Configure Nunjucks
 nunjucks.configure('views', {
     autoescape: true,
     express: app
 })
 
 app.get('/', (req, res) => {
-    res.render('index.njk')
+    res.render('home')
+})
+
+app.get('/authentication', (req, res) => {
+    res.render('authentication', {
+        alerts: [
+            {
+                type: 'success',
+                message: 'Hello World!'
+            }
+        ]
+    })
 })
 
 app.listen(port, () => {
