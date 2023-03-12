c428cb208c618c11c09d6122053bf3ae6e47b3c6
Ayan Das Karmakar <coderayan1411@gmail.com>
2023-03-12 23:26:56 +0530

    Created User model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model, SchemaTypes } = require('mongoose');
+
+const validateEmail = (email) => {
+    let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Name is required'
+    },
+    email: {
+        type: SchemaTypes.Email,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: 'Password address is required'
+    }
+});
+
+const UserModel = model('User', UserSchema);
+
+module.exports = UserModel
